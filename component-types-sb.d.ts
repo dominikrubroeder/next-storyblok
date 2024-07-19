a1ed9305449b2673ef58475fe27e5afe9746d7c7
Dominik Rubröder <dominik.rubroeder@icloud.com>
2024-07-19 21:29:34 +0200

    feat(DX) – Prepare production build with `npm run prepare-build`

--- component-types-sb.d.ts
+++ component-types-sb.d.ts
@@ -1,4 +1,4 @@
-import {StoryblokStory} from 'storyblok-generate-ts'
+import { StoryblokStory } from "storyblok-generate-ts";
 
 export interface PageStoryblok {
   body?: (PageStoryblok | RichtextFieldStoryblok)[];
